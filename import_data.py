e5d4dd651094c0ee2dc93baa19ecb79a15b46527
rommelbquinto <quinto.rommel@gmail.com>
2025-05-29 10:42:55 +0800

    Update import_data.py

--- import_data.py
+++ import_data.py
@@ -3,7 +3,7 @@ import os, psycopg2
 def import_csvs():
     conn = psycopg2.connect(os.getenv('DATABASE_URL'))
     cur = conn.cursor()
-    for fname in os.listdir('main'):
+    for fname in os.listdir('data'):
         if fname.endswith('.csv'):
             table = fname.replace('.csv', '')
             cur.execute(
